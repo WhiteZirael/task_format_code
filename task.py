7884c7aebd73e3dd7fcf257f89b25827b0e9b2fb
Timur Ashrapov <ashrapovtd@gmail.com>
2026-03-12 18:56:51 +0300

    Added names and single quotes

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
-def add(x, y):
-    return x + y
+def add(first_number, second_number):
+    return first_number + second_number
 
 
-x = 10
-y = 5
-print("Сумма чисел: ", add(x, y))
+first_number = 10
+second_number = 5
+print('Сумма чисел: ', add(first_number, second_number))
